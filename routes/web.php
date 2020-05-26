5136dc06cc2afad52065fa92937681a84404b6f9
xiaobai <123@qq.com>
2020-05-26 09:39:59 +0800

    2020年5月26日09:39:57

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/promote', 'PromoteController@index');  //新歌推广页面
 Route::get('/promote/song', 'PromoteController@song');//新歌推广ajax获取数据
 
 Route::get('/qrCodeUrl', 'OrderController@qrCodeUrl');//扫二维码后访问的链接页面
-Route::get('/notifyUrl', 'OrderController@notifyUrl');//支付成功乐刷异步通知地址
+Route::post('/notifyUrl', 'OrderController@notifyUrl');//支付成功乐刷异步通知地址
 Route::get('/jumpUrl', 'OrderController@jumpUrl');//支付成功页面跳转地址
 Route::get('/queryOrder', 'OrderController@queryOrder');//查询订单
 
