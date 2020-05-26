fc61a6455c60b85d1e150c3b46eaec72145b9878
xiaobai <123@qq.com>
2020-05-26 09:44:35 +0800

    2020年5月26日09:44:32

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -192,18 +192,18 @@ class OrderController extends Controller
     //支付成功，乐刷通知地址
     public function notifyUrl()
     {
-
-        if(isset($_POST['status']) && ($_POST['status']== 2)){
-//            file_put_contents('1.txt',"订单号:".$_POST['third_order_id']."\r\n".
-//                "金额:".$_POST['amount']."\r\n".
-//                "支付时间:".$_POST['pay_time']."\r\n".
-//                "支付类型:".$_POST['pay_way']."\r\n"
-//            );
-            file_put_contents('1.txt',"回调成功");
-        }else{
-//            file_put_contents('1.txt',"订单号:");
-            return response()->json(['code' => 500, 'msg' => '异步通知失败', 'data' => null]);
-        }
+        file_put_contents('1.txt',"回调成功");
+//        if(isset($_POST['status']) && ($_POST['status']== 2)){
+////            file_put_contents('1.txt',"订单号:".$_POST['third_order_id']."\r\n".
+////                "金额:".$_POST['amount']."\r\n".
+////                "支付时间:".$_POST['pay_time']."\r\n".
+////                "支付类型:".$_POST['pay_way']."\r\n"
+////            );
+//            file_put_contents('1.txt',"回调成功");
+//        }else{
+////            file_put_contents('1.txt',"订单号:");
+//            return response()->json(['code' => 500, 'msg' => '异步通知失败', 'data' => null]);
+//        }
     }
     
     //支付成功，直接跳转地址 乐刷会带参数跳转leshuaOrderId，result
