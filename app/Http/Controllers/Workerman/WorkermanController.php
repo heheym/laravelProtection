6449ea73f38bb85bb1ddf80da174bd07f5c46e44
xiaobai <123@qq.com>
2020-06-02 18:58:03 +0800

    2020年6月2日18:58:02

--- app/Http/Controllers/Workerman/WorkermanController.php
+++ app/Http/Controllers/Workerman/WorkermanController.php
@@ -11,8 +11,8 @@ class WorkermanController extends Controller
     //
     public function index($arr=[])
     {
-        $ordersn = DB::table('ordersn')->where('key','SZGA8BOPZLBB')->first();
-        $data = ['srvkey'=>$ordersn->key,'KtvBoxid'=>$ordersn->KtvBoxid,'pay_time'=>$ordersn->pay_time,'leshua_order_id'=>$ordersn->leshua_order_id,'amount'=>$ordersn->amount];
+//        $ordersn = DB::table('ordersn')->where('key','SZGA8BOPZLBB')->first();
+//        $data = ['srvkey'=>$ordersn->key,'KtvBoxid'=>$ordersn->KtvBoxid,'pay_time'=>$ordersn->pay_time,'leshua_order_id'=>$ordersn->leshua_order_id,'amount'=>$ordersn->amount];
         /// 建立socket连接到内部推送端口
         $client = stream_socket_client('tcp://47.106.155.48:82', $errno, $errmsg, 1);
 //        $client = stream_socket_client('tcp://127.0.0.1:82', $errno, $errmsg, 1);
@@ -20,7 +20,7 @@ class WorkermanController extends Controller
 //        $data = array('code'=>200, 'msg'=>'支付成功','data'=>$KtvBoxid);
 // 发送数据，注意5678端口是Text协议的端口，Text协议需要在数据末尾加上换行符
 //        var_dump($client);
-        fwrite($client, json_encode($data,JSON_UNESCAPED_UNICODE)."\n");
+        fwrite($client, json_encode($arr,JSON_UNESCAPED_UNICODE)."\n");
 // 读取推送结果
         echo(fread($client, 8192));
         fclose($client);
