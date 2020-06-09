2daa1cfa67facb4d8b8fa29fcbe91c12c55e0313
xiaobai <123@qq.com>
2020-06-09 17:08:39 +0800

    2020年6月9日16:35:15

--- app/Http/Controllers/Workerman/WorkermanController.php
+++ app/Http/Controllers/Workerman/WorkermanController.php
@@ -47,10 +47,10 @@ class WorkermanController extends Controller
         $abc = fread($client, 8192);
         if($abc=='success'){
             echo 1;
-            echo $abc;
+            var_dump($abc); ;
         }else{
             echo 2;
-            echo $abc;
+            var_dump($abc); ;
         }
 
         fclose($client);
