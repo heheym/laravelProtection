aa7bcda7bd5d1e67f4e02a660df760d05759ba6c
xiaobai <123@qq.com>
2020-06-12 14:46:15 +0800

    2020年6月12日14:46:13

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -18,9 +18,10 @@ class HomeController extends Controller
 //            ->row(Dashboard::title())
             ->row(function (Row $row) {
 
-//                $row->column(4, function (Column $column) {
-//                    $column->append(Dashboard::environment());
-//                });
+
+                $row->column(4, function (Column $column) {
+                    $column->append($_SERVER['SERVER_ADDR']);
+                });
 
 //                $row->column(4, function (Column $column) {
 //                    $column->append(Dashboard::extensions());
