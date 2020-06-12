15048ecd4ce2491461081c9010adcd00cd14305d
xiaobai <123@qq.com>
2020-06-12 15:02:28 +0800

    2020年6月12日15:02:26

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -20,7 +20,10 @@ class HomeController extends Controller
 
 
                 $row->column(4, function (Column $column) {
-                    $column->append($_SERVER['HTTP_HOST']);
+                    $externalContent = file_get_contents('http://checkip.dyndns.com/');
+                    preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
+                    $externalIp = $m[1];
+                    $column->append($externalIp);
                 });
 
 //                $row->column(4, function (Column $column) {
