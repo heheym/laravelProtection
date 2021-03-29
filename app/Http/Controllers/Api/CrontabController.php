f94e955a8e6322a6013088fbb9f507bfa9ca6fc5
heheym <2547252250@qq.com>
2021-03-29 17:11:08 +0800

    2021年3月29日17:11:07

--- app/Http/Controllers/Api/CrontabController.php
+++ app/Http/Controllers/Api/CrontabController.php
@@ -20,9 +20,10 @@ class CrontabController extends Controller
         if(count($ordersn)>0){
             foreach($ordersn as $v){
                 if($v->option==1){
-                    $data = ['func'=>'place_push','srvkey'=>$v->key,
+                    $place = DB::table('place')->where('key',$v->key)->first();
+                    $data = ['func'=>'place_pay','srvkey'=>$v->key,
                         'pay_time'=>$v->pay_time,'leshua_order_id'=>$v->leshua_order_id,
-                        'amount'=>$v->amount,'openid'=>$v->openid];
+                        'amount'=>$v->amount,'openid'=>$v->openid,'balanceSum'=>$place->balanceSum];
                 }else{
                     $data = ['func'=>'push_pay','srvkey'=>$v->key,'KtvBoxid'=>$v->KtvBoxid,
                         'pay_time'=>$v->pay_time,'leshua_order_id'=>$v->leshua_order_id,
