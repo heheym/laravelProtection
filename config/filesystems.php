45ce2ec00afb1520927d00fb7288c2329b4b8506
heheym <2547252250@qq.com>
2021-04-12 16:32:27 +0800

    2021年4月12日16:24:07

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,7 +54,7 @@ return [
             'driver' => 'local',
             'root' => public_path('uploads'),
             'visibility' => 'public',
-            'url' => env('APP_URL').'/uploads/files',
+            'url' => env('APP_URL').'/uploads/',
         ],
 
         'public' => [
